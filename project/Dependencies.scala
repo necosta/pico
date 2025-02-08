56288cf68a70e3f90e741229ed522d1ad04a2a7c
Nelson Costa <nelson.costa85@gmail.com>
2025-02-08 21:23:45 +0000

    Add first FileOps methods for compression and decompression

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,11 @@ import sbt.*
 object Dependencies {
 
   private val CatsVersion      = "3.5.7"
+  private val Fs2Version       = "3.11.0"
   private val ScalatestVersion = "3.2.19"
 
   lazy val libs: Seq[ModuleID] = Seq(
+    "co.fs2"        %% "fs2-io"      % Fs2Version,
     "org.typelevel" %% "cats-effect" % CatsVersion
   )
 
---
+++ src/main/scala/com/necosta/pico/file/FileOps.scala
@@ -0,0 +1,44 @@
+package com.necosta.pico.file
+
+import cats.effect.IO
+import fs2.io.file.{Files, Path}
+
+import java.io.File
+import java.nio.charset.StandardCharsets
+
+trait Ops {
+  // Compress a given txt file into pico file
+  def compress(): IO[Unit]
+
+  // Decompress a given pico file into txt file
+  def decompress(): IO[Unit]
+}
+
+class FileOps(sourceFileName: String) extends Ops {
+
+  private val FileExtension = "pico"
+  private val sourceFile    = new File(sourceFileName)
+  private val targetFile    = new File(s"${sourceFile.getPath}.$FileExtension")
+
+  def compress(): IO[Unit] = {
+    Files[IO]
+      .readAll(Path(sourceFile.getPath))
+      .chunkN(1024, allowFewer = true)
+      .evalTapChunk(x => IO.println(new String(x.toArray, StandardCharsets.UTF_8)))
+      .flatMap(fs2.Stream.chunk)
+      .through(Files[IO].writeAll(Path(targetFile.getPath)))
+      .compile
+      .drain
+  }
+
+  def decompress(): IO[Unit] = {
+    Files[IO]
+      .readAll(Path(sourceFile.getPath))
+      .chunkN(1024, allowFewer = true)
+      .evalTapChunk(x => IO.println(new String(x.toArray, StandardCharsets.UTF_8)))
+      .flatMap(fs2.Stream.chunk)
+      .through(Files[IO].writeAll(Path(targetFile.getPath)))
+      .compile
+      .drain
+  }
+}
